56afcd47e494db7d607100a295d3f1ba1f020aed
Tyler Mayes <timmymayes@gmail.com>
2026-07-20 23:32:34 -0700

    setup dashboard.el so I have a clean startup

---
+++ configbackups/early-init.el
@@ -0,0 +1,6 @@
+;;; early-init.el --- Pre-init frame configuration -*- lexical-binding: t -*-
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(provide 'early-init)
+;;; early-init.el ends here
--- configbackups/init.el
+++ configbackups/init.el
@@ -116,9 +116,14 @@
   (add-to-list 'mixed-pitch-fixed-pitch-faces 'org-block))
 
 (use-package nano-theme
-  :config
+  :defer t)
+
+(defun my/apply-theme-and-faces (&optional frame)
+  "Apply theme, Nord face attributes, and transparency.
+  Safe to call from frame-creation hooks."
+  (when frame (select-frame frame))
   (load-theme 'nano-dark t)
-  ;; font-lock-faces
+  (set-face-attribute 'default nil :font "Roboto Mono" :height 140)
   (set-face-attribute 'font-lock-keyword-face nil :foreground "#81A1C1" :weight 'bold)
   (set-face-attribute 'font-lock-type-face nil :foreground "#EBCB8B" :weight 'normal)
   (set-face-attribute 'font-lock-function-name-face nil :foreground "#6dBFB8" :weight 'bold)
@@ -126,8 +131,29 @@
   (set-face-attribute 'font-lock-comment-face nil :foreground "#616E88" :slant 'italic)
   (set-face-attribute 'font-lock-builtin-face nil :foreground "#B48EAD")
   (set-face-attribute 'font-lock-constant-face nil :foreground "#D08770")
+  ;; Transparency: 60% opaque focused / 45% opaque unfocused
+  (set-frame-parameter (selected-frame) 'alpha '(60 . 45)))
 
-  )
+;; Also cover future frames created without the server hook
+(add-to-list 'default-frame-alist '(alpha . (60 . 45)))
+
+(if (daemonp)
+    (add-hook 'server-after-make-frame-hook #'my/apply-theme-and-faces)
+  (my/apply-theme-and-faces))
+
+;; (use-package nano-theme
+;;   :config
+;;   (load-theme 'nano-dark t)
+;;   ;; font-lock-faces
+;;   (set-face-attribute 'font-lock-keyword-face nil :foreground "#81A1C1" :weight 'bold)
+;;   (set-face-attribute 'font-lock-type-face nil :foreground "#EBCB8B" :weight 'normal)
+;;   (set-face-attribute 'font-lock-function-name-face nil :foreground "#6dBFB8" :weight 'bold)
+;;   (set-face-attribute 'font-lock-string-face nil :foreground "#A3BE8C")
+;;   (set-face-attribute 'font-lock-comment-face nil :foreground "#616E88" :slant 'italic)
+;;   (set-face-attribute 'font-lock-builtin-face nil :foreground "#B48EAD")
+;;   (set-face-attribute 'font-lock-constant-face nil :foreground "#D08770")
+
+;;   )
 ;; define org stylizing colors
 (with-eval-after-load 'org
   (set-face-attribute 'org-code nil
@@ -162,6 +188,11 @@
   (term-mode . nano-modeline-term-mode)
   (messages-buffer-mode . nano-modeline-message-mode))
 
+(setq display-time-format "%H:%M"
+      display-time-default-load-average nil
+      display-time-24hr-format t)
+(display-time-mode 1)
+
 (with-eval-after-load 'org
   (set-face-attribute 'org-block nil
                       :foreground 'unspecified
@@ -183,6 +214,9 @@
 
 ;; testing if this works to set transparency to full on startup
 (global-set-key (kbd "M-<f12>") 'my/toggle-transparency)
+
+(add-to-list 'default-frame-alist '(alpha . (80 . 60)))
+(set-frame-parameter nil 'alpha '(80 . 60))
 
 (message "UI Config Section Finished Loading")
 
@@ -1933,6 +1967,7 @@
  (flag Holiday)
  (info Note)
  (star Goals)
+ (star Goal)
  (gift Reward)
  (map-marker Review)
  (archive Archive))
@@ -3973,6 +4008,130 @@ Version: 2018-06-04 2021-03-16 2022-03-05"
 (define-key global-map (kbd "M-n") 'xah-end-of-line-or-block)
 
 (message "Xah Lee section loaded")
+
+(use-package dashboard
+  :ensure t
+  :after (all-the-icons org)
+  :init
+  (setq dashboard-startup-banner "~/EMACS Wordmark-02.png")
+  (setq dashboard-center-content t)
+  (setq dashboard-vertically-center-content t)
+  (setq dashboard-icon-type 'all-the-icons)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  :config
+  (setq dashboard-footer-messages
+      	'("While any text editor can save your files, only Emacs can save your soul"
+      	  "Vi Vi Vi, the editor of the beast"
+          ))
+  (setq dashboard-banner-logo-title "It's time to work.")
+  
+  (defvar my/dashboard-bookmark-exclusions
+    '("org-capture-last-stored-marker"
+      "org-refile-last-stored"
+      "org-capture-last-stored"))
+  (advice-add 'bookmark-all-names :filter-return
+              (lambda (names)
+                (seq-remove (lambda (n) (member n my/dashboard-bookmark-exclusions))
+                            names)))
+
+  (defun my/dashboard-insert-tasks (list-size)
+    "Add the list of LIST-SIZE task entries (CATEGORY=\"Task\")."
+    (require 'org-agenda)
+    (let ((dashboard-match-agenda-entry "CATEGORY=\"Task\""))
+      (dashboard-insert-section
+       "Tasks:"
+       (dashboard-agenda--sorted-agenda)
+       list-size
+       'my-tasks
+       "t"
+       `(lambda (&rest _)
+          (let ((file (get-text-property 0 'dashboard-agenda-file ,el))
+        	(point (get-text-property 0 'dashboard-agenda-loc ,el)))
+            (funcall dashboard-agenda-action file point)))
+       (format "%s" el))))
+  
+  (defun my/dashboard-insert-projects (list-size)
+    "Add the list of LIST-SIZE project entries (CATEGORY=\"Proj\")."
+    (require 'org-agenda)
+    (let ((dashboard-match-agenda-entry "CATEGORY=\"Proj\"")
+          (dashboard-filter-agenda-entry 'dashboard-filter-agenda-by-todo))
+      (dashboard-insert-section
+       "Projects:"
+       (dashboard-agenda--sorted-agenda)
+       list-size
+       'my-projects
+       "p"
+       `(lambda (&rest _)
+          (let ((file (get-text-property 0 'dashboard-agenda-file ,el))
+    		(point (get-text-property 0 'dashboard-agenda-loc ,el)))
+            (funcall dashboard-agenda-action file point)))
+       (format "%s" el))))
+
+
+
+  (defun my/dashboard-filter-goal-by-90-day-deadline ()
+    "Include entry if it has a deadline within the next 90 days and is not done.
+    An entry is included if this function returns nil and excluded if returns point."
+    (let ((deadline (org-get-deadline-time (point)))
+          (cutoff (time-add (current-time) (* 86400 90))))
+      (unless (and (not (org-entry-is-done-p))
+                   (not (org-in-archived-heading-p))
+                   deadline
+                   (time-less-p deadline cutoff))
+    	(point))))
+
+  (defun my/dashboard-insert-goals (list-size)
+    "Add the list of LIST-SIZE goal entries with deadlines within 90 days."
+    (require 'org-agenda)
+    (let ((dashboard-match-agenda-entry "CATEGORY=\"Goals\"")
+          (dashboard-filter-agenda-entry 'my/dashboard-filter-goal-by-90-day-deadline))
+      (dashboard-insert-section
+       "Goals (next 90 days):"
+       (dashboard-agenda--sorted-agenda)
+       list-size
+       'my-goals
+       "g"
+       `(lambda (&rest _)
+          (let ((file (get-text-property 0 'dashboard-agenda-file ,el))
+    		(point (get-text-property 0 'dashboard-agenda-loc ,el)))
+            (funcall dashboard-agenda-action file point)))
+       (format "%s" el))))
+
+  (defvar my/dashboard-prefontify-files
+    '("~/Orgfiles/tasks.org"
+      "~/Orgfiles/config.org"
+      "~/Orgfiles/Productivity/Goals/Goals.org"
+      "~/Orgfiles/SEAS.org")
+    "Files to fully fontify before dashboard extracts entries.
+Ensures TODO keyword decorations from org-modern render correctly
+in the dashboard buffer.")
+
+  (add-hook 'dashboard-before-initialize-hook #'my/dashboard-prefontify-files)
+
+  (defun my/dashboard-prefontify-files ()
+    "Load and fully fontify files in `my/dashboard-prefontify-files'."
+    (dolist (file my/dashboard-prefontify-files)
+      (when (file-exists-p file)
+	(with-current-buffer (find-file-noselect file)
+          (font-lock-ensure)))))
+
+
+  (add-to-list 'dashboard-item-generators '(my-tasks . my/dashboard-insert-tasks)) 
+  (add-to-list 'dashboard-item-generators '(my-goals . my/dashboard-insert-goals))
+  (add-to-list 'dashboard-item-generators '(my-projects . my/dashboard-insert-projects))
+
+
+
+
+  (setq dashboard-items '((recents . 5)
+                          (bookmarks . 5)
+                          (my-tasks . 5)
+                          (my-goals . 5)
+                          (my-projects . 5)))
+  (dashboard-setup-startup-hook))
+
+(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 
 ;;Make org links open in same window
 (setq org-link-frame-setup
