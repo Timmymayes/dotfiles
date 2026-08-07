e909969e3cbaf4c3cf3d946fc979b4a11c1a2ee3
Tyler Mayes <timmymayes@gmail.com>
2026-08-07 13:51:41 -0700

    default directory setup for ~/ in early init

--- configbackups/early-init.el
+++ configbackups/early-init.el
@@ -1,5 +1,12 @@
 ;;; early-init.el --- Pre-init frame configuration -*- lexical-binding: t -*-
 
+(setq-default default-directory "~/")
+
+(dolist (buf '("*scratch*" "*Messages*"))
+  (when (get-buffer buf)
+    (with-current-buffer buf
+      (setq default-directory "~/"))))
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(font . "Roboto Mono-14"))
 (provide 'early-init)
